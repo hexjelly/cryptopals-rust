3ad9313cf671264622ff865fe9b6fea5c7b90d87
Roger Andersen <hexjelly@hexjelly.com>
2016-11-05 09:33:39 +0100

    Move challenge text out of test file

--- tests/lib.rs
+++ tests/lib.rs
@@ -5,26 +5,12 @@ mod tests {
     use cryptopals::set_01::{ challenge_01, challenge_02 };
 
     #[test]
-    // Convert hex to base64
-    // The string:
-    //  49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d
-    // Should produce:
-    //  SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t
     fn hex_to_base64 () {
         let test = challenge_01::hex_to_base64("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d");
         assert_eq!(test, "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t");
     }
 
     #[test]
-    // Fixed XOR
-    // Write a function that takes two equal-length buffers and produces their XOR combination.
-    //
-    // If your function works properly, then when you feed it the string:
-    //  1c0111001f010100061a024b53535009181c
-    // ... after hex decoding, and when XOR'd against:
-    //  686974207468652062756c6c277320657965
-    // ... should produce:
-    //  746865206b696420646f6e277420706c6179
     fn fixed_xor () {
         let test = challenge_02::fixed_xor("1c0111001f010100061a024b53535009181c", "686974207468652062756c6c277320657965");
         assert_eq!(test, "746865206b696420646f6e277420706c6179".to_string().to_uppercase());
@@ -34,5 +20,11 @@ mod tests {
     #[should_panic]
     fn fixed_xor_uneven () {
         let _test = challenge_02::fixed_xor("1c1c", "efa0c1");
+    }
+
+    #[test]
+    fn single_byte_xor_cipher () {
+        let key = challenge_03::single_byte_xor_cipher("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736");
+        assert_eq!();
     }
 }
