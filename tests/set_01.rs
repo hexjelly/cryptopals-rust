6f8ba15459fcddc99d840d9627e825a442187a63
Roger Andersen <hexjelly@hexjelly.com>
2016-11-09 10:47:44 +0100

    Add Set 1, Challenge 6 text and data

--- tests/set_01.rs
+++ tests/set_01.rs
@@ -3,7 +3,7 @@ mod test_data;
 
 use cryptopals::*;
 use std::cmp::Ordering;
-use test_data::CHALLENGE_03_CONTENT;
+use test_data::{ CHALLENGE_03_CONTENT, CHALLENGE_06_CONTENT };
 
 // Convert hex to base64
 // The string:
@@ -102,3 +102,22 @@ fn repeating_key_xor_returns_correct_value () {
     assert_eq!(test, "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272\
             a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f".to_string().to_uppercase());
 }
+
+// Break repeating-key XOR
+// There's a file here. It's been base64'd after being encrypted with repeating-key XOR.
+//
+// Decrypt it.
+// Here's how:
+//
+// Let KEYSIZE be the guessed length of the key; try values from 2 to (say) 40.
+// Write a function to compute the edit distance/Hamming distance between two strings. The Hamming distance is just the number of differing bits. The distance between:
+//  this is a test
+// and
+//  wokka wokka!!!
+// is 37. Make sure your code agrees before you proceed.
+// For each KEYSIZE, take the first KEYSIZE worth of bytes, and the second KEYSIZE worth of bytes, and find the edit distance between them. Normalize this result by dividing by KEYSIZE.
+// The KEYSIZE with the smallest normalized edit distance is probably the key. You could proceed perhaps with the smallest 2-3 KEYSIZE values. Or take 4 KEYSIZE blocks instead of 2 and average the distances.
+// Now that you probably know the KEYSIZE: break the ciphertext into blocks of KEYSIZE length.
+// Now transpose the blocks: make a block that is the first byte of every block, and a block that is the second byte of every block, and so on.
+// Solve each block as if it was single-character XOR. You already have code to do this.
+// For each block, the single-byte XOR key that produces the best looking histogram is the repeating-key XOR key byte for that block. Put them together and you have the key.
